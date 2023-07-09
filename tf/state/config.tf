bef29728b6d9db407cdb01c16de3034f82a51154
Andrew Clemons <andrew.clemons@gmail.com>
2023-07-09 11:51:15 +0900

    Init terraform state.

---
+++ tf/state/config.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket  = "caffe-terraform"
+    key     = "state/infra.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+
+    dynamodb_table = "caffe-terraform"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+
+  required_version = "1.5.2"
+
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
