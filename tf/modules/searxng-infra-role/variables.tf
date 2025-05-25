a4583aaef7e6ee2234f6d4dc3144702d5bf9d7fa
Andrew Clemons <andrew.clemons@gmail.com>
2025-05-25 17:55:22 +0900

    Remove unused searxng role.

--- tf/modules/searxng-infra-role/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  type        = string
-  description = "Prefix for all resources."
-}
-
-variable "role_name" {
-  type        = string
-  description = "The name of the role"
-}
